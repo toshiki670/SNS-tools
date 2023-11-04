c5741c9de013495ee509a28b7188faf5f99d81ae
Toshiki <bushy.trivial.0o@icloud.com>
2023-11-04 19:45:29 +0900

    Remove template 'create_view'

--- src/app.rs
+++ src/app.rs
@@ -39,54 +39,84 @@ pub fn App() -> Html {
                     greet_msg.set(new_msg);
                 });
 
-                || {}
-            },
-            name2,
-        );
-    }
 
-    let greet = {
-        let name = name.clone();
-        let greet_input_ref = greet_input_ref.clone();
-        Callback::from(move |e: SubmitEvent| {
-            e.prevent_default();
-            name.set(
-                greet_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
-        })
-    };
+// #[derive(Serialize, Deserialize)]
+// struct GreetArgs<'a> {
+//     name: &'a str,
+// }
 
-    html! {
-        <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://yew.rs" target="_blank">
-                    <img src="public/yew.png" class="logo yew" alt="Yew logo"/>
-                </a>
-            </div>
+// #[function_component]
+// pub fn App() -> Html {
+//     let greet_input_ref = use_node_ref();
 
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
+//     let name = use_state(|| String::new());
 
-            <p>
-                {"Recommended IDE setup: "}
-                <a href="https://code.visualstudio.com/" target="_blank">{"VS Code"}</a>
-                {" + "}
-                <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">{"Tauri"}</a>
-                {" + "}
-                <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">{"rust-analyzer"}</a>
-            </p>
+//     let greet_msg = use_state(|| String::new());
+//     {
+//         let greet_msg = greet_msg.clone();
+//         let name = name.clone();
+//         let name2 = name.clone();
+//         use_effect_with_deps(
+//             move |_| {
+//                 spawn_local(async move {
+//                     if name.is_empty() {
+//                         return;
+//                     }
 
-            <form class="row" onsubmit={greet}>
-                <input id="greet-input" ref={greet_input_ref} placeholder="Enter a name..." />
-                <button type="submit">{"Greet"}</button>
-            </form>
+//                     let args = to_value(&GreetArgs { name: &*name }).unwrap();
+//                     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//                     let new_msg = invoke("greet", args).await.as_string().unwrap();
+//                     greet_msg.set(new_msg);
+//                 });
 
-            <p><b>{ &*greet_msg }</b></p>
-        </main>
-    }
-}
+//                 || {}
+//             },
+//             name2,
+//         );
+//     }
+
+//     let greet = {
+//         let name = name.clone();
+//         let greet_input_ref = greet_input_ref.clone();
+//         Callback::from(move |e: SubmitEvent| {
+//             e.prevent_default();
+//             name.set(
+//                 greet_input_ref
+//                     .cast::<web_sys::HtmlInputElement>()
+//                     .unwrap()
+//                     .value(),
+//             );
+//         })
+//     };
+
+//     html! {
+//         <main class="container">
+//             <div class="row">
+//                 <a href="https://tauri.app" target="_blank">
+//                     <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
+//                 </a>
+//                 <a href="https://yew.rs" target="_blank">
+//                     <img src="public/yew.png" class="logo yew" alt="Yew logo"/>
+//                 </a>
+//             </div>
+
+//             <p>{"Click on the Tauri and Yew logos to learn more."}</p>
+
+//             <p>
+//                 {"Recommended IDE setup: "}
+//                 <a href="https://code.visualstudio.com/" target="_blank">{"VS Code"}</a>
+//                 {" + "}
+//                 <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">{"Tauri"}</a>
+//                 {" + "}
+//                 <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">{"rust-analyzer"}</a>
+//             </p>
+
+//             <form class="row" onsubmit={greet}>
+//                 <input id="greet-input" ref={greet_input_ref} placeholder="Enter a name..." />
+//                 <button type="submit">{"Greet"}</button>
+//             </form>
+
+//             <p><b>{ &*greet_msg }</b></p>
+//         </main>
+//     }
+// }
