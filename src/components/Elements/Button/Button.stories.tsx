8ae6ac2dc8fec03706df3fa3e3876a4863c247ba
Toshiki <bushy.trivial.0o@icloud.com>
2023-11-17 02:31:26 +0900

    Update stories

--- src/components/Elements/Button/Button.stories.tsx
+++ src/components/Elements/Button/Button.stories.tsx
@@ -2,15 +2,14 @@ import { type Meta, type StoryObj } from "@storybook/react";
 
 import { Button } from "./Button";
 
-const meta: Meta<typeof Button> = {
+export default {
   title: "Components/Elements/Button",
   component: Button,
   parameters: {
     controls: { expanded: true },
   },
-};
+} satisfies Meta<typeof Button>;
 
-export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
