a51204d780aca583d2cc6a894323b6108951e5fe
Toshiki <bushy.trivial.0o@icloud.com>
2023-11-18 22:22:30 +0900

    Remove comment

--- src-tauri/src/router/app.rs
+++ src-tauri/src/router/app.rs
@@ -1,7 +1,6 @@
 use super::RouterBuilder;
 
 pub(crate) fn mount() -> RouterBuilder {
-    // getAppNameをエンドポイントとし、文字列で"rspc Test Project"を返す
     RouterBuilder::new().query("getAppName", |t| {
         t(|_: (), s: String| Ok(format!("rspc Test Project: {}", s)))
     })
