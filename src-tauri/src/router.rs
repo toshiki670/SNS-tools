827026ca62f79c101d4050aee65c28a50a28c5bf
Toshiki <bushy.trivial.0o@icloud.com>
2023-11-18 22:19:09 +0900

    Fix code

--- src-tauri/src/router.rs
+++ src-tauri/src/router.rs
@@ -9,12 +9,12 @@ pub(crate) fn mount() -> Arc<Router> {
     let config = rspc::Config::new().set_ts_bindings_header("/* eslint-disable */");
 
     let config = config.export_ts_bindings(
-        std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/types/rspc/bindings.ts"), // ②
+        std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/types/rspc/bindings.ts"),
     );
 
-    <Router>::new()
+    Router::new()
         .config(config)
-        .merge("app.", app::mount()) // ③
+        .merge("app.", app::mount())
         .build()
         .arced()
 }
--- src-tauri/src/router/app.rs
+++ src-tauri/src/router/app.rs
@@ -2,7 +2,7 @@ use super::RouterBuilder;
 
 pub(crate) fn mount() -> RouterBuilder {
     // getAppNameをエンドポイントとし、文字列で"rspc Test Project"を返す
-    <RouterBuilder>::new().query("getAppName", |t| {
+    RouterBuilder::new().query("getAppName", |t| {
         t(|_: (), s: String| Ok(format!("rspc Test Project: {}", s)))
     })
 }
