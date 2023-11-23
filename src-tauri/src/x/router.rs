888410f3895a4026ff16cbb88a6f24a38455ffcc
Toshiki <bushy.trivial.0o@icloud.com>
2023-11-23 17:37:18 +0900

    Move Old code

--- src-tauri/src/x/router.rs
+++ src-tauri/src/x/router.rs
@@ -1,14 +1,19 @@
 use crate::base_ctx::BaseCtx;
 use crate::router::RouterBuilder;
 
-use super::client;
-
 pub fn router() -> RouterBuilder {
     RouterBuilder::new()
         .query("getAppName", |t| {
-            t(|_: BaseCtx, _: ()| async { client::call().await.unwrap() })
+            t(|_: BaseCtx, _: ()| async { call().await.unwrap() })
         })
         .query("getAppName2", |t| {
             t(|_: BaseCtx, s: String| Ok(format!("rspc Test Project: {}", s)))
         })
+}
+
+pub async fn call() -> Result<String, reqwest::Error> {
+    let url = "https://httpbin.org/ip";
+    let contents = reqwest::get(url).await?.text().await?;
+    println!("{:?}", contents);
+    Ok(contents)
 }
