5950aade7f1d0f3ad081f02c79ac1fe49c78d62e
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-09 14:01:44 +0900

    Change return json

--- src-tauri/src/x/command.rs
+++ src-tauri/src/x/command.rs
@@ -5,13 +5,13 @@ use tauri::{command, State};
 use super::client::XClient;
 
 #[command]
-pub async fn x_get_api<'r>(client: State<'r, XClient>) -> Result<Value, String> {
+pub async fn x_get_api<'r>(client: State<'r, XClient>) -> Result<Value, Value> {
     match client.tauri_api().await {
         Ok(response) => Ok(json!(response)),
         Err(err) => {
             json!(err.to_string());
             error!("{:?}", err);
-            Err("Something Err".to_string())
+            Err(json!({"Error": "API"}))
         }
     }
 }
