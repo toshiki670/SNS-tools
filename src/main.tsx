b36ba4ef3b2af71a14c2fded22fd704c0eaf7770
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-16 11:54:15 +0900

    ignore eslint message

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 
+// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
