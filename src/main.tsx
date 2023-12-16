bfcf626785410ff5bd3d0ed5b21e0b15637dc4ec
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-16 11:56:03 +0900

    Add i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
+import "./i18n/configs";
 
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
