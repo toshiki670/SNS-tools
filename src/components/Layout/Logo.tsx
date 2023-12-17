9884c0c8b3944643f5b2ae37b5f56b9c4c6deb1c
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-17 18:30:50 +0900

    Fix path

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -4,7 +4,7 @@ import logo from "@/assets/react.svg";
 
 export const Logo = (): JSX.Element => {
   return (
-    <Link className="flex items-center text-white" to=".">
+    <Link className="flex items-center text-white" to="/">
       <img className="h-8 w-auto" src={logo} alt="Workflow" />
       <span className="text-xl text-black font-semibold">Passwords</span>
     </Link>
