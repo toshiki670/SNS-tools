744324dad97febdd97a1a3d6ee3d052866560342
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-24 01:32:26 +0900

    Old validation

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,6 +1,7 @@
-use log::error;
+use log::{error, info};
 use serde_json::{json, Value};
 use tauri::command;
+use tauri::regex::Regex;
 
 use crate::settings;
 
@@ -17,4 +18,55 @@ pub async fn submit_settings() -> Result<Value, String> {
     } else {
         Err(i18n_path("submit_settings", "failed"))
     }
+}
+
+#[command]
+pub fn validate_system_password(password: &str) -> Result<(), String> {
+    match password.len() {
+        0 => Err(i18n_path("validate_system_password", "empty")),
+        1..=8 => Err(i18n_path("validate_system_password", "less_then_8")),
+        _ => {
+            let a: &str = r#"[a-z]"#;
+
+            let regexes = vec![r#"[a-z]"#, r#"[A-Z]"#, r#"\d"#, r#"[^a-zA-Z0-9]"#];
+            if regexes
+                .iter()
+                .find(|r| false == Regex::new(r).unwrap().is_match(password))
+                .is_none()
+            {
+                Ok(())
+            } else {
+                Err(i18n_path("validate_system_password", "week_password"))
+            }
+        }
+    }
+}
+
+#[command]
+pub fn validate_system_current_password(password: &str) -> Result<(), String> {
+    Ok(())
+}
+
+#[command]
+pub fn validate_system_confirm_password(newpsw: &str, confirm: &str) -> Result<(), String> {
+    if newpsw == confirm {
+        Ok(())
+    } else {
+        Err(i18n_path("validate_system_confirm_password", "not_match"))
+    }
+}
+
+#[command]
+pub fn update_system_password(current: &str, newpsw: &str, confirm: &str) -> String {
+    validate_system_password(current).unwrap();
+    validate_system_current_password(current).unwrap();
+    validate_system_password(newpsw).unwrap();
+    validate_system_password(confirm).unwrap();
+    validate_system_confirm_password(newpsw, confirm).unwrap();
+
+    info!("{}", current);
+    info!("{}", newpsw);
+    info!("{}", confirm);
+
+    i18n_path("update_system_password", "success")
 }
