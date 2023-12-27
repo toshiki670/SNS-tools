fb18cc7d2b61cdf793d5077903b017c836efa321
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-27 18:35:27 +0900

    Rename

--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -13,11 +13,11 @@ export const Landing = (): JSX.Element => {
 
   return (
     <>
-      <Head description="Welcome to SNS Tools" />
+      <Head description="Welcome to Passwords" />
       <div className="bg-white h-[100vh] flex items-center">
         <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">SNS Tools</span>
+            <span className="block">Passwords</span>
           </h2>
           <img src={logo} alt="react" />
           <p>Showcasing Best Practices For Building React Applications</p>
