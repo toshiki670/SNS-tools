d92fc61e65a13a19f4a3cc4c640550f1b7c1922e
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-27 11:55:21 +0900

    Rename

--- src/i18n/configs.ts
+++ src/i18n/configs.ts
@@ -1,15 +1,17 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import translation_en_us from "./en-US.json";
-import translation_ja_jp from "./ja-JP.json";
+import { getSettings } from "@/tauri/command";
+
+import translationEnUS from "./EnUS.json";
+import translationJaJP from "./JaJP.json";
 
 const resources = {
-  "en-US": {
-    translation: translation_en_us,
+  EnUS: {
+    translation: translationEnUS,
   },
-  "ja-JP": {
-    translation: translation_ja_jp,
+  JaJP: {
+    translation: translationJaJP,
   },
 };
 
@@ -17,7 +19,7 @@ void i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en-US", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: (await getSettings()).general.language, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
