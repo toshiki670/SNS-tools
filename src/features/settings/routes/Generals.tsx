587235d507f83d8898b13af8cbc43e76dc360f03
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-28 20:43:45 +0900

    Remove old comment

--- src/features/settings/routes/Generals.tsx
+++ src/features/settings/routes/Generals.tsx
@@ -1,24 +1,7 @@
-// import { useState } from "react";
-
-// import {
-//   FormControl,
-//   TextField,
-//   Container,
-//   Grid,
-//   Box,
-//   Typography,
-// } from "@mui/material";
-
-// import { Spinner } from '@/components/Elements'
 import { useTranslation } from "react-i18next";
 
 import { ContentLayout } from "@/components/Layout";
 import { SystemPassword, Language } from "../components/Elements";
-
-// import { formatDate } from '@/utils/format';
-// import { Button } from "@/components/Elements";
-
-// import { invoke } from "@tauri-apps/api/tauri";
 
 export const Generals = (): JSX.Element => {
   const { t } = useTranslation("settings");
