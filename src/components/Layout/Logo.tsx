08497fe921b812f82024bd480961d8925328e891
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-28 20:23:59 +0900

    i18n appname

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,10 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 import logo from "@/assets/react.svg";
 
 export const Logo = (): JSX.Element => {
+  const { t } = useTranslation();
   return (
     <div className="flex items-center text-white">
       <img className="h-8 w-auto" src={logo} alt="Workflow" />
-      <span className="text-xl text-black font-semibold">Passwords</span>
+      <span className="text-xl text-black font-semibold">{t("appName")}</span>
     </div>
   );
 };
