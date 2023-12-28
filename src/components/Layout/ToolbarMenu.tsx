670e664cfa4a44dcb752e347562d5a0b333e0827
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-28 20:56:10 +0900

    Support i18n for ToolberMenu

--- src/components/Layout/ToolbarMenu.tsx
+++ src/components/Layout/ToolbarMenu.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-
+import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
 import { IconButton, Menu, MenuItem } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 
 export const ToolbarMenu = (): JSX.Element => {
+  const { t } = useTranslation();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const handleMenu = (event: React.MouseEvent<HTMLElement>): void => {
     setAnchorEl(event.currentTarget);
@@ -42,8 +43,14 @@ export const ToolbarMenu = (): JSX.Element => {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Lock</MenuItem>
-        <MenuItem component={NavLink} to={"./settings/generals"} onClick={handleClose}>Settings</MenuItem>
+        <MenuItem onClick={handleClose}>{t("features.lock")}</MenuItem>
+        <MenuItem
+          component={NavLink}
+          to={"./settings/generals"}
+          onClick={handleClose}
+        >
+          {t("features.settings")}
+        </MenuItem>
       </Menu>
     </>
   );
