04e8d59b465d8a9eddee74b5e7d668d02afdf585
Toshiki <bushy.trivial.0o@icloud.com>
2023-12-31 23:40:33 +0900

    Fix code

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { App } from "./App";
 import "./styles.css";
 import "./i18n/configs";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
