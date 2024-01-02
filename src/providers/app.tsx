2dc40c6c50d224e153d6839ee1340cf05a4b6c92
Toshiki <bushy.trivial.0o@icloud.com>
2024-01-02 12:43:22 +0900

    Add theme provider

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,15 +1,29 @@
 import type React from "react";
+import { useState } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { type PaletteMode } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 interface AppProviderProps {
   children: React.ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps): JSX.Element => {
+  const [mode, setMode] = useState<PaletteMode>("dark");
+  const theme = createTheme({
+    palette: {
+      mode,
+    },
+  });
+
   return (
-    <HelmetProvider>
-      <BrowserRouter>{children}</BrowserRouter>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <HelmetProvider>
+        <BrowserRouter>{children}</BrowserRouter>
+      </HelmetProvider>
+    </ThemeProvider>
   );
 };
