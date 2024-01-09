414434e72882ed8148f0fdfcf3cf47beadfa1513
Toshiki <bushy.trivial.0o@icloud.com>
2024-01-09 18:50:57 +0900

    Add Diesel

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{api::path::app_data_dir, Manager};
+
 mod command;
+mod db;
 mod settings;
+
+use db::ConnectionPool;
 
 fn main() {
     std::env::set_var("RUST_LOG", log::Level::Trace.to_string());
     env_logger::init();
 
     tauri::Builder::default()
+        .setup(|app: &mut tauri::App| {
+            let app_path =
+                app_data_dir(&app.config()).expect("Failed to get application data path.");
+            if !app_path.exists() {
+                std::fs::create_dir_all(&app_path).unwrap();
+            }
+
+            let connection_pool = db::establish_connection(app_path.clone());
+            db::run_migration(&connection_pool);
+            app.manage::<ConnectionPool>(connection_pool);
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             command::submit_settings,
             command::get_settings,
