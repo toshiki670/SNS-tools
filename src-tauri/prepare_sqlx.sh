2561dc0a2e8076d9535d657f659b732a193f3908
Toshiki <bushy.trivial.0o@icloud.com>
2024-01-20 18:00:04 +0900

    Migrate to makefile

--- src-tauri/prepare_sqlx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sqlx db create --database-url="sqlite://passwords.db"
-sqlx mig run --database-url="sqlite://passwords.db"
-cargo sqlx prepare --database-url="sqlite://passwords.db"
-
-rm passwords.db
