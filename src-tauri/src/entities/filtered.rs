1de108d1e5d1cefe7fb9cf2cbfcb1b75670a0bf3
Toshiki <bushy.trivial.0o@icloud.com>
2024-01-23 00:26:38 +0900

    Fix struct

--- src-tauri/src/entities/filtered.rs
+++ src-tauri/src/entities/filtered.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub struct Filtered<T>(T);
+
+/// When the Filtered type is displayed with `std::fmt::Display`, it will show as `[FILTERED]`.
+pub struct Filtered<T>(pub T);
 
 impl<T> std::fmt::Display for Filtered<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
